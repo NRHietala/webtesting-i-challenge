987aa171546ad1320b23f5a0849c0d24c1ee1709
Nathan Hietala <Nrhietala@gmail.com>
2021-02-24 16:58:49 -0600

    working on tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,29 @@
+function success(item) {
+  return item.enhancement === 20 ? item : item.enhancement++;
+}
+
+function fail(item) {
+  if (item.enhancement < 15) {
+    item.durability - 5;
+  } else if (item.enhancement >= 15) {
+    item.durability -= 10;
+    item.enhancement--;
+  }
+  return item;
+}
+
+function repair(item) {
+  item.durability = 100;
+  return item;
+}
+
+function get(item) {
+  return { ...item };
+}
+
 module.exports = {
   success,
   fail,
   repair,
   get,
 };
-
-function success(item) {
-  return { ...item };
-}
-
-function fail(item) {
-  return { ...item };
-}
-
-function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
-}
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,24 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+const thunderfury = {
+  name: "Thunderfury",
+  enhancement: 14,
+  durability: 90,
+};
+
+describe("Enhancer Exists", () => {
+  it("sanity", () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe("Repair Items", () => {
+  it("Restore item durability", () => {
+    enhancer.repair(thunderfury);
+    expect(thunderfury.durability).toBe(100);
+  });
+
+  describe("Successfuly Enhanced Items", () => {
+    it("");
+  });
+});
